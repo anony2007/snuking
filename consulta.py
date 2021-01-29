093737daf6677053750df1ff50c984e511f12021
Snuking <78182577+Snuking@users.noreply.github.com>
2021-01-28 22:57:49 -0300

    Add files via upload
    ꧁ঔৣ☬ 🆂🅽🆄🅺🅸🅽🅶 ☬ঔৣ꧂
    
    
    Copyright (c) 2021 Snuking.
    
    Snuking, a ferramenta para consulta, trata-se de um sistema avançado, em Python, que ”puxa" dados de: IP, CPF, CEP, CNPJ, placa e telefone.
    
    Os scripts possuem características autorais e, seu uso para quaisquer meios sem vínculo pedagógico ou educacional, é proibido. Não possui intenção comercial, portanto, não está a venda dos direitos.
    
    As informações obtidas: IP: Seu endereço de IP; Cidade; Região; País; Abreviação do país; Capital do país; População do país; Moeda; Nome da moeda; Código da região; Código postal; Código do país; Código do país ISO3; Área do país; País TLD; Código área; Código do continente; União Européia; Latitude; Longitude; Fuso horário; Código de Chamada; línguas; ASN; ORG; Deslocamento UTF; Versão; Link localização Google Maps;
    
    CPF: Nome completo; Data de nascimento; Nome da Mae; Endereço; Complemento; Bairro; Cidade; CEP; Gera e valida CPF;
    
    CEP: ENDEREÇO; COMPLEMENTO; BAIRRO; CIDADE; ESTADO; IBGE; GIA; DDD; SIAFI;
    
    CNPJ: Completo. (100%)
    
    Placa: Ano; Data; Modelo; Ano do modelo; Cor; Marca; Roubo/furto; Situação; Chassi; UF; Município; Modificada em; Alarme atualizado; Mensagem de retorno; Código de retorno;
    
    Telefone: Formato internacional; Formato local; País; Cidade/Estado; Operadora; Fuso horário; Validação; Scan com Numverify.com; Operadora; Tipo de linha;
    
    Instalação: Para instalar no seu Termux, siga os comandos:
    
    apt update && apt upgrade apt instala python git clone https://github.com/Snuking/Snuking/ ls cd snuking python install.py python consulta.py
    
    © Snuking, 2021

---
+++ consulta.py
@@ -0,0 +1,113 @@
+#Snuking/Somos uma legião. 2021 ©
+
+import requests, time
+import os
+
+
+os.system("clear")
+R='\033[1;31m'; B='\033[1;34m'; C='\033[1;37m'; Y='\033[1;33m'; G='\033[1;32m'; RT='\033[;0m'
+
+code_info = C + '[' + Y + 'i' + C + '] '
+code_details = C + '[' + G + '*' + C + '] '
+code_result = C + '[' + G + '+' + C + '] '
+code_error = C + '[' + R + '!' + C + '] '
+
+print(f'''{C}
+              ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+        ▓▓▓▓██░░░░░░░░░░░░░░░░▓▓██▓▓
+      ██░░░░                    ░░░░▓▓
+    ▓▓░░                            ░░▓▓
+  ██░░                                ░░██
+  ██    ░░░░▓▓░░░░      ░░░░░░░░░░░░    ▓▓
+  ▓▓  ▓▓░░▒▒▓▓▓▓▓▓░░    ░░▓▓▓▓▓▓▒▒░░▓▓  ▓▓
+  ▓▓██          ▓▓██    ████          ██▓▓
+  ▓▓            ░░░░    ░░░░          ░░▓▓
+  ▓▓▒▒  ▒▒▒▒▓▓▒▒░░░░    ░░░░▒▒▒▒▓▓▒▒  ▒▒▓▓
+  ▓▓░░▓▓▓▓▓▓▓▓▓▓▓▓░░    ░░▓▓▓▓▓▓▓▓▓▓▓▓░░▓▓
+  ▓▓░░  ░░░░░░░░  ▓▓    ██  ░░░░░░░░  ░░▓▓
+  ▓▓  ░░░░        ▓▓    ██        ░░░░  ▓▓
+  ▓▓░░░░░░░░      ▓▓    ██      ░░░░░░░░▓▓
+  ▓▓░░░░░░░░            ▓▓        ░░░░  ██
+  ▓▓░░▓▓        ▓▓        ▓▓        ▓▓░░▓▓
+  ▓▓░░▓▓▓▓        ▓▓░░░░▓▓        ▓▓▓▓░░▓▓
+  ▓▓░░  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ░░▓▓
+  ▓▓░░░░░░▓▓▓▓▓▓▓▓▓▓    ▓▓▓▓▓▓▓▓▓▓░░░░░░▓▓
+    ▒▒  ░░░░░░░░░░░░░░░░▒▒▒▒▒▒░░▒▒░░░░▒▒
+      ▓▓    ░░░░░░░░░░░░░░░░░░░░  ░░▓▓
+      ▓▓░░    ░░░░░░██▓▓░░░░░░    ░░▓▓
+        ▓▓░░        ▓▓▓▓        ░░▓▓
+          ▓▓░░    ░░▓▓▓▓░░    ░░▓▓
+            ▓▓░░  ░░▓▓▓▓░░  ░░▓▓
+              ██░░  ▓▓▓▓  ░░▒▒
+                ▓▓▓▓▓▓▓▓▓▓▓▓
+
+             SNUKING / ANONYMOUS
+'''); time.sleep(1.5)
+os.system("clear")
+
+#Snuking/Somos uma legião. 2021 ©
+
+def main():
+  os.system("clear")
+  print(f'''{G}
+\n
+.----.______
+|           |
+|    ___________
+|   /          /
+|  /          /
+| /          /
+|/__________/
+{C}
+ ''')
+
+  print("\n" + code_info + "Menu.")
+
+  print(f'''
+{C}[{G}i{C}] Formas de operação: 
+
+[{G}1{C}] IP.
+[{G}2{C}] CPF.
+[{G}3{C}] CEP.
+[{G}4{C}] CNPJ.
+[{G}5{C}] Placa.
+[{G}6{C}] Telefone.
+[{G}7{C}] {R}Sair.{C}
+''')
+  tool=input(f'{C}[{G}+{C}] Selecione a forma de operação:{B} ')
+  if tool == "1":
+     os.system('clear')
+     import ip
+     ip.main()
+  elif tool == "2":
+     os.system('clear')
+     import cpf
+     cpf.main()
+  elif tool == "3":
+     os.system('clear')
+     import cep
+     cep.main()
+  elif tool == "4":
+     os.system('clear')
+     import cnpj
+     cnpj.main()
+  elif tool == "5":
+     os.system('clear')
+     import placa
+     placa.main()
+  elif tool == "6":
+     os.system('clear')
+     import telefone
+     telefone.main()
+  elif tool == "7":
+     os.system("clear")
+     print(f'\n{G}Somos uma legião.{C}\n')
+     exit()
+  else:
+     os.system("clear")
+     print(f'{C}[{R}-{C}] Seleção inválida.')
+     time.sleep(0.2)
+     main()
+main()
+
+#Snuking/Somos uma legião. 2021 ©
---
+++ install.py
@@ -0,0 +1,4 @@
+#Snuking/Somos uma legião. 2021 ©
+
+import os
+os.system("pip install -r requirements.txt")
